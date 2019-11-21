6e6c59ed4567d62b7842de19b247f25077d5c58e
wangpengzhe <wangpengzhe@jd.com>
2019-11-21 18:17:39 +0800

    modify code

---
+++ src/main/java/com/wpz/algorithm/BTreeDemo.java
@@ -0,0 +1,88 @@
+package com.wpz.algorithm;
+
+import java.util.Scanner;
+
+/**
+ * @author wangpengzhe
+ * @description 二叉搜索树
+ * @date 2019/11/21 16:07
+ * @copyright Copyright 2017-2027 JD.COM All Right Reserved
+ */
+public class BTreeDemo {
+
+    TreeNode root = null;
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 10, 20, 16};
+        BTreeDemo tree = new BTreeDemo();
+        for (int i = 0; i < arr.length; i++) {
+            tree.insert(arr[i]);
+        }
+
+        tree.afterPrint(tree.root);
+    }
+
+    class TreeNode {
+
+        int data;
+        TreeNode leftNode;
+        TreeNode rightNode;
+
+        TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    private void insert(int data) {
+        if (root == null) {
+            root = new TreeNode(data);
+            return;
+        }
+        TreeNode tmp = root;
+        while (tmp != null) {
+            if (tmp.data < data) {
+                if (tmp.rightNode == null) {
+                    tmp.rightNode = new TreeNode(data);
+                    return;
+                }
+                tmp = tmp.rightNode;
+            } else {
+                if (tmp.leftNode == null) {
+                    tmp.leftNode = new TreeNode(data);
+                    return;
+                }
+                tmp = tmp.leftNode;
+            }
+        }
+    }
+
+    private void prePrint(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + "->");
+        prePrint(root.leftNode);
+        prePrint(root.rightNode);
+    }
+
+    private void midPrint(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        midPrint(root.leftNode);
+        System.out.print(root.data + "->");
+        midPrint(root.rightNode);
+    }
+
+    private void afterPrint(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        afterPrint(root.leftNode);
+        afterPrint(root.rightNode);
+        System.out.print(root.data + "->");
+    }
+
+
+
+}
