403a8de26e214906d25cf8091e3bfe62bd36cb3b
wangpz <751920167@qq.com>
2019-12-24 23:37:18 +0800

    modify code

---
+++ src/main/java/com/wpz/StringExercise/SplitWord.java
@@ -0,0 +1,37 @@
+package com.wpz.StringExercise;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: wpz
+ * @Desctription: 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ * @Date: Created in 2019/12/24 21:15
+ */
+public class SplitWord {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("apple", "pen");
+        System.out.println(wordBreak("applepenapple", list));
+    }
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : wordDict) {
+            map.put(word, 1);
+        }
+        boolean[] dp = new boolean[s.length() + 1];
+
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && map.get(s.substring(j, i)) != null) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
--- src/main/java/com/wpz/StringExercise/SplitWord2.java
+++ src/main/java/com/wpz/StringExercise/SplitWord2.java
@@ -1,26 +1,24 @@
-package com.wpz.leetcode;
-
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
+package com.wpz.StringExercise;
 
 import java.util.*;
 
 /**
- * @author wangpengzhe
- * @description 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，在字符串中增加空格来构建一个句子，使得句子中所有的单词都在词典中。返回所有这些可能的句子。
- * @date 2019/12/10 14:55
+ * @Author: wpz
+ * @Desctription: 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，在字符串中增加空格来构建一个句子，使得句子中所有的单词都在词典中。返回所有这些可能的句子。
+ * @Date: Created in 2019/12/24 22:04
  */
-public class L140 {
-
+public class SplitWord2 {
     public static void main(String[] args) {
-        System.out.println(wordBreak("pineapplepenapple", Arrays.asList("apple", "pen", "applepen", "pine", "pineapple")));
+        List<String> list = Arrays.asList("cat", "cats", "and", "sand", "dog");
+        System.out.println(wordBreak("catsanddog", list));
+
     }
 
     static List<List<String>> lists = new ArrayList<>();
 
     public static List<String> wordBreak(String s, List<String> wordDict) {
         List<String> res = new ArrayList<>();
-        if (CollectionUtils.isEmpty(wordDict) || StringUtils.isEmpty(s)) {
+        if (wordDict == null || wordDict.size() == 0) {
             return res;
         }
         Map<String, Integer> map = new HashMap<>();
@@ -30,6 +28,7 @@ public class L140 {
         if (!isSplit(s, wordDict)) {
             return res;
         }
+
         backtrace(s, map, new ArrayList<>());
         for (int i = 0; i < lists.size(); i++) {
             String str = "";
@@ -38,11 +37,26 @@ public class L140 {
             }
             res.add(str.substring(0, str.length() - 1));
         }
-
         return res;
     }
 
-    public static boolean isSplit(String s, List<String> dict) {
+    private static void backtrace(String s, Map<String, Integer> map, List<String> list) {
+        if (s.length() == 0) {
+            lists.add(new ArrayList<>(list));
+        }
+
+        String tmp;
+        for (int i = 1; i <= s.length(); i++) {
+            tmp = s.substring(0, i);
+            if (map.get(tmp) != null) {
+                list.add(tmp);
+                backtrace(s.substring(i, s.length()), map, list);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+    private static boolean isSplit(String s, List<String> dict) {
         int len = s.length();
         boolean[] now = new boolean[len + 1];
         now[0] = true;
@@ -59,21 +73,4 @@ public class L140 {
         }
         return now[len];
     }
-
-    private static void backtrace(String str, Map<String, Integer> map, List<String> list) {
-        if (str == null || str.length() == 0) {
-            lists.add(new ArrayList<>(list));
-            return;
-        }
-        String s;
-        for (int i = 1; i <= str.length(); i++) {
-            s = str.substring(0, i);
-            if (map.get(s) != null) {
-                list.add(s);
-                backtrace(str.substring(i, str.length()), map, list);
-                list.remove(list.size() - 1);
-            }
-        }
-    }
-
 }
